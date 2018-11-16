f81e0452d86a439486ee991c4e4eaca32774d894
jplaceres <33101350+jplaceres@users.noreply.github.com>
2018-11-16 14:10:24 -0500

    updated stuff
    updated stuff

--- app/src/main/java/com/example/jplac/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jplac/myapplication/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.jplac.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
+public class MainActivity extends CursorAdapter {
+
+    //adds layout variables
+    private TextView title;
+    private TextView prompt;
+    private Context activity;
+
+    public CustomAdapter(Context context, Cursor c){
+        super(context,c);
+        activity = context;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.list);
+
+
+
+
+
     }
 
 
