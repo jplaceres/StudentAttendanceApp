8976911a8a3cf4a88e7fb5b5f9e8d6934003e0ed
jplaceres <33101350+jplaceres@users.noreply.github.com>
2018-11-20 16:09:08 -0500

    updates

--- app/src/main/java/com/example/jplac/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jplac/myapplication/MainActivity.java
@@ -1,52 +1,142 @@
 package com.example.jplac.myapplication;
 
-import android.app.Fragment;
-import android.content.Context;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import android.content.Context;
-import android.database.Cursor;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.CursorAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.quickstart.auth.R;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-//import com.google.firebase.quickstart.database.R;
-//import com.google.firebase.quickstart.database.java.models.Post;
-//import com.google.firebase.quickstart.database.java.models.User;
+public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "EmailPassword";
 
-public class MainActivity extends AppCompatActivity{
-    private Button addCourse;
-    private FirebaseDatabase studentDatabase;
-    private DatabaseReference myRef;
+    private TextView title;
+    private TextView prompt;
+
+    private EditText emailET;
+    private EditText passwordET;
+
+    private Button signIn;
+    private TextView createUser;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
 
-        myRef = FirebaseDatabase.getInstance().getReference();
+        //get variables from XML
+        title = findViewById(R.id.textView1);
+        prompt = findViewById(R.id.textView3);
+        emailET = (EditText) findViewById(R.id.editText);
+        passwordET = (EditText) findViewById(R.id.editText2);
+        signIn = (Button) findViewById(R.id.button1);
+        createUser = findViewById(R.id.textView2);
 
-        ListView list = (ListView) findViewById(R.id.list);
+        mAuth = FirebaseAuth.getInstance();
 
+        final String email = emailET.getText().toString();
+        final String password = passwordET.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        // ...EXCLUDE??
+                    }
+                });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"SignIn pressed",Toast.LENGTH_LONG).show();
+
+                mAuth.signInWithEmailAndPassword(email,password);
+
+                Intent intent = new Intent(MainActivity.this, MyCourseList.class);
+                startActivity(intent);
+            }
+        });
+
+        createUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"createNew pressed",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SetupInitial.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
+    //checks if user is already signed in
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
 
 
+
+    public void updateUI(FirebaseUser user){
+        Toast.makeText(MainActivity.this,"updateUI",Toast.LENGTH_LONG).show();
+    }
+
+//    private void updateUI(FirebaseUser user) {
+//        hideProgressDialog();
+//        if (user != null) {
+//            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+//                    user.getEmail(), user.isEmailVerified()));
+//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+//
+//            findViewById(R.id.emailPasswordButtons).setVisibility(View.GONE);
+//            findViewById(R.id.emailPasswordFields).setVisibility(View.GONE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+//
+//            findViewById(R.id.verifyEmailButton).setEnabled(!user.isEmailVerified());
+//
+//        } else {
+//
+//            mStatusTextView.setText(R.string.signed_out);
+//            mDetailTextView.setText(null);
+//
+//            findViewById(R.id.emailPasswordButtons).setVisibility(View.VISIBLE);
+//            findViewById(R.id.emailPasswordFields).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+//
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/jplac/myapplication/MyCourseList.java
@@ -0,0 +1,52 @@
+package com.example.jplac.myapplication;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+//import com.google.firebase.quickstart.database.R;
+//import com.google.firebase.quickstart.database.java.models.Post;
+//import com.google.firebase.quickstart.database.java.models.User;
+
+
+public class MyCourseList extends AppCompatActivity{
+    private Button addCourse;
+    private FirebaseDatabase studentDatabase;
+    private DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        ListView list = (ListView) findViewById(R.id.list);
+
+    }
+
+
+
+
+}
