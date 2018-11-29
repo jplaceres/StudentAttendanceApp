24322f9d20d8f8f07f11224769ac0b2e956ab667
Nick Pellegrino <nickpellegrino0@gmail.com>
2018-11-29 16:10:38 -0500

    Courses being taken are now stored in arraylist in courselist class

--- app/src/main/java/com/example/jplac/myapplication/MyCourseList.java
+++ app/src/main/java/com/example/jplac/myapplication/MyCourseList.java
@@ -6,15 +6,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import android.content.Context;
-import android.database.Cursor;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -29,11 +20,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -44,9 +30,6 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-//import com.google.firebase.quickstart.database.R;
-//import com.google.firebase.quickstart.database.java.models.Post;
-//import com.google.firebase.quickstart.database.java.models.User;
 
 
 public class MyCourseList extends AppCompatActivity{
